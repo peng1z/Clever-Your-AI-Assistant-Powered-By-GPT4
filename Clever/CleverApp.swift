cb7d4310615647acbc6be823a262b55106de3212
Patrick Zhang <pzh1pzh@gmail.com>
2024-05-25 09:41:55 -0700

    Initial Commit

---
+++ Clever/CleverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleverApp.swift
+//  Clever
+//
+//  Created by Peng Zhang on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CleverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
